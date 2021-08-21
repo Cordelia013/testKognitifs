bddeb87455eb2f2e31e088273dac59c2eabebc57
Cordelia013 <64865633+Cordelia013@users.noreply.github.com>
2021-08-21 13:46:58 +0200

    Delete base.sql

--- backend/base.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `FullCalendar`; 
-CREATE USER IF NOT EXISTS cordelia_guims@localhost IDENTIFIED BY 'BOUBOU'; 
-GRANT ALL ON `FullCalendar`.* TO cordelia_guims@localhost; 
